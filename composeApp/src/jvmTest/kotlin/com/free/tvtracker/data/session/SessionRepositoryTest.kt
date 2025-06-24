90e7b84f7a6e135cac33e8942436d4d36b9e0e0e
joao <joaosardinha18@hotmail.com>
2025-06-24 22:36:55 +0100

    more

---
+++ composeApp/src/jvmTest/kotlin/com/free/tvtracker/data/session/SessionRepositoryTest.kt
@@ -0,0 +1,97 @@
+package com.free.tvtracker.data.session
+
+import com.free.tvtracker.data.common.LocalSqlDataProvider
+import com.free.tvtracker.expect.data.TvHttpClientEndpoints
+import com.free.tvtracker.user.response.SessionApiModel
+import com.free.tvtracker.user.response.SessionApiResponse
+import com.free.tvtracker.user.response.UserApiModel
+import io.mockk.called
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SessionRepositoryTest {
+    val sessionStore: SessionStore = mockk(relaxed = true)
+
+    @Test
+    fun `GIVEN no session WHEN init THEN anon session is created`() = runTest {
+        val httpClient: TvHttpClientEndpoints = mockk {
+            coEvery { createAnonUser() } returns SessionApiResponse(
+                SessionApiModel(
+                    "token1",
+                    UserApiModel("", 1, "", "", true, true)
+                )
+            )
+        }
+        val localStore: LocalSqlDataProvider = mockk {
+            every { getSession() } returns null
+        }
+        val sut = SessionRepository(httpClient, localStore, sessionStore, mockk())
+        sut.createAnonSession()
+        coVerify(exactly = 1) { httpClient.createAnonUser() }
+    }
+
+    @Test
+    fun `GIVEN no session WHEN init THEN new session is kept in reference`() = runTest {
+        val httpClient: TvHttpClientEndpoints = mockk {
+            coEvery { createAnonUser() } returns SessionApiResponse(
+                SessionApiModel(
+                    "token1",
+                    UserApiModel("", 1, "", "", true, true)
+                )
+            )
+        }
+        val localStore: LocalSqlDataProvider = mockk(relaxed = true) {
+            every { getSession() } returns null
+        }
+        val sut = SessionRepository(httpClient, localStore, sessionStore, mockk())
+        sut.createAnonSession()
+        verify { sessionStore.token = "token1" }
+    }
+
+    @Test
+    fun `GIVEN a session WHEN init THEN no new session is created`() = runTest {
+        val httpClient: TvHttpClientEndpoints = mockk()
+        val localStore: LocalSqlDataProvider = mockk {
+            every { getSession() } returns SessionClientEntity("token1", "", 1, "", "", true, true)
+        }
+        val sut = SessionRepository(httpClient, localStore, sessionStore, mockk())
+        sut.loadSession()
+        coVerify { httpClient wasNot called }
+        verify { sessionStore.token = "token1" }
+    }
+
+    @Test
+    fun `GIVEN a session WHEN init THEN locally stored session is available`() = runTest {
+        val httpClient: TvHttpClientEndpoints = mockk()
+        val localStore: LocalSqlDataProvider = mockk {
+            every { getSession() } returns SessionClientEntity("token1", "", 1, "", "", true, true)
+        }
+        val sut = SessionRepository(httpClient, localStore, sessionStore, mockk())
+        sut.loadSession()
+        verify { sessionStore.token = "token1" }
+    }
+
+    @Test
+    fun `GIVEN new anon session is created THEN its stored`() = runTest {
+        val httpClient: TvHttpClientEndpoints = mockk {
+            coEvery { createAnonUser() } returns SessionApiResponse(
+                SessionApiModel(
+                    "token1",
+                    UserApiModel("", 1, "", "", true, true)
+                )
+            )
+        }
+        val localStore: LocalSqlDataProvider = mockk(relaxed = true)
+        val sut = SessionRepository(httpClient, localStore, sessionStore, mockk())
+        sut.createAnonSession()
+        coVerify(exactly = 1) { localStore.saveSession(withArg {
+            assertEquals("token1", it.token)
+        }) }
+    }
+}
--- composeApp/src/jvmTest/kotlin/com/free/tvtracker/ui/watching/WatchingViewModelTest.kt
+++
@@ -1,5 +0,0 @@
-package com.free.tvtracker.ui.watching
-
-class WatchingViewModelTest {
-
-}
